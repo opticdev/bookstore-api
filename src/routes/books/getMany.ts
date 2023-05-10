70b122cc40ccfc602511b855d83d85c15c169f24
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-05-10 09:45:00 +0100

    Add sort query parameters to GET /books

--- src/routes/books/getMany.ts
+++ src/routes/books/getMany.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { Static } from "@sinclair/typebox";
+import { Type, Static } from "@sinclair/typebox";
 
 import * as Books from "../../services/books";
 import { PaginatedTypeBox } from "../../services/paginated";
@@ -7,18 +7,38 @@ import { PaginatedTypeBox } from "../../services/paginated";
 const GetBooksResponse = PaginatedTypeBox(Books.BookResponse);
 type GetBooksResponse = Static<typeof GetBooksResponse>;
 
+const GetBooksQuery = Type.Object({
+  sort_order: Type.Optional(
+    Type.String({
+      enum: ["asc", "desc"],
+    })
+  ),
+  sort_key: Type.Optional(
+    Type.String({
+      enum: ["name", "created_at", "updated_at"],
+    })
+  ),
+});
+type GetBooksQuery = Static<typeof GetBooksQuery>;
+
 export const registerGetBooks = (app: FastifyInstance) => {
   app.get<{
     Reply: GetBooksResponse;
+    Querystring: GetBooksQuery;
   }>(
     `/books`,
     {
       schema: {
+        querystring: GetBooksQuery,
         response: { 200: GetBooksResponse },
       },
     },
     (request, reply) => {
-      const books = Books.getMany();
+      const sort = {
+        order: request.query.sort_order ?? "asc",
+        key: request.query.sort_key ?? "name",
+      } as Parameters<typeof Books.getMany>["0"];
+      const books = Books.getMany(sort);
       reply.code(200).send({
         has_more_data: false,
         data: books,
