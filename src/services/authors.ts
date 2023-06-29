a96f268dbe923a275254ed8e91d795b6170811cf
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-06-29 14:29:35 -0400

    feat: add date of birth to authors

--- src/services/authors.ts
+++ src/services/authors.ts
@@ -9,6 +9,7 @@ export type AuthorRequest = Static<typeof AuthorRequest>;
 export const AuthorResponse = Type.Object({
   id: Type.String(),
   name: Type.String(),
+  date_of_birth: Type.Optional(Type.String()),
   created_at: Type.String(),
   updated_at: Type.String(),
 });
@@ -18,24 +19,28 @@ const Authors: AuthorResponse[] = [
   {
     id: "6nTxAFM5ck4Hob77hGQoL",
     name: "Jane Austen",
+    date_of_birth: "1775-12-16T12:00:00.000Z",
     created_at: "2023-04-22T17:17:41.326Z",
     updated_at: "2023-04-22T17:17:41.326Z",
   },
   {
     id: "NjpTwgmENj11rGdUgpCQ9",
     name: "F. Scott Fitzgerald",
+    date_of_birth: "1896-09-24T12:00:00.000Z",
     created_at: "2023-03-22T10:11:51.421Z",
     updated_at: "2023-03-22T10:11:51.421Z",
   },
   {
     id: "AcSwiQryWBeQqcNBqBg43",
     name: "Harper Lee",
+    date_of_birth: "1926-04-28T12:00:00.000Z",
     created_at: "2023-04-01T07:11:01.701Z",
     updated_at: "2023-04-01T07:11:01.701Z",
   },
   {
     id: "tNpOpQZbxytxTxDT15GQy",
     name: "George Orwell",
+    date_of_birth: "1903-06-25T12:00:00.000Z",
     created_at: "2023-03-11T21:19:08.600Z",
     updated_at: "2023-03-11T21:19:08.600Z",
   },
